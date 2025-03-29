3d6a046e36464879f093313cedf282060dbeb082
Trung Anh <anhthtservice@gmail.com>
2025-03-29 13:02:38 +0700

    feat: implement info device and location

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
+import * as Device from 'expo-device';
+import * as Location from 'expo-location';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import * as Location from 'expo-location';
 
 const HomeScreen: FunctionComponent = () => {
   const [location, setLocation] = useState<Location.LocationObject>();
@@ -16,8 +17,6 @@ const HomeScreen: FunctionComponent = () => {
 
       const location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-
-      console.log('Location: ', location);
     };
 
     getPermissions();
@@ -25,6 +24,10 @@ const HomeScreen: FunctionComponent = () => {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
       <Text className="text-4xl font-semibold text-muted">Check location</Text>
+      <Text className="text-4xl font-semibold text-muted">
+        {Device.manufacturer}: {Device.modelName}
+      </Text>
+      <Text className="text-4xl font-semibold text-muted">{Device.osBuildId}</Text>
     </SafeAreaView>
   );
 };
